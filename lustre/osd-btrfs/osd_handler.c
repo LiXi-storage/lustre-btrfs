50e63fbf9355cba97798eb71af47ed016a10acfa
Li Xi <lixi@ddn.com>
2014-11-28 22:16:26 -0500

    LU-0000 osd-btrfs: add some functions to osd-btrfs
    This patch adds some functions to osd-btrfs. Now we can mount
    a btrfs filestem formated by mkfs.lustre as an OST. However, since
    no object interfaces is implemented yet, no operations could be
    done on that OST. This patch also hacks some function in OFD, MGC
    and target in order to walk around some object operations.
    
    Wang Shilong: Rebasing and compiling fixes
    
    Signed-off-by: Li Xi <lixi@ddn.com>
    Signed-off-by: Wang Shilong <wshilong@ddn.com>
    Change-Id: Ia24e2031c18a6aca07a22d86a4a1e2306f466fdd

--- lustre/osd-btrfs/osd_handler.c
+++ lustre/osd-btrfs/osd_handler.c
@@ -50,17 +50,775 @@
 /* XATTR_{REPLACE,CREATE} */
 #include <linux/xattr.h>
 
-#include <obd.h>
+/*
+ * struct OBD_{ALLOC,FREE}*()
+ * OBD_FAIL_CHECK
+ */
+#include <obd_support.h>
+/* struct ptlrpc_thread */
+#include <lustre_net.h>
+#include <lustre_fid.h>
+/* process_config */
+#include <lustre_param.h>
+
+/* llo_* api support */
+#include <md_object.h>
+#include <lustre_quota.h>
+
+/* class_process_* */
+#include <obd_class.h>
+
+/* lu_* api support */
+#include <lu_object.h>
+
+#include "osd_internal.h"
+
+/*
+ * Concurrency: shouldn't matter.
+ */
+static int osd_sync(const struct lu_env *env, struct dt_device *d)
+{
+	int rc;
+
+	CDEBUG(D_CACHE, "syncing OSD %s\n", LUSTRE_OSD_BTRFS_NAME);
+	/* LIXI TODO: add actual sync function */
+	rc = 0;
+	CDEBUG(D_CACHE, "synced OSD %s: rc = %d\n",
+	       LUSTRE_OSD_BTRFS_NAME, rc);
+
+	return rc;
+}
+
+static void osd_umount(const struct lu_env *env, struct osd_device *o)
+{
+	ENTRY;
+
+	if (o->od_mnt != NULL) {
+		shrink_dcache_sb(osd_sb(o));
+		osd_sync(env, &o->od_dt_dev);
+
+		mntput(o->od_mnt);
+		o->od_mnt = NULL;
+	}
+
+	EXIT;
+}
+
+static int osd_mount(const struct lu_env *env,
+		     struct osd_device *o, struct lustre_cfg *cfg)
+{
+	const char		*name  = lustre_cfg_string(cfg, 0);
+	const char		*dev  = lustre_cfg_string(cfg, 1);
+	const char              *opts;
+	unsigned long            page, s_flags, lmd_flags = 0;
+	struct page             *__page;
+	struct file_system_type *type;
+	char                    *options = NULL;
+	char			*str;
+	int			 option_size = PAGE_SIZE;
+#ifdef LIXI
+	struct osd_thread_info	*info = osd_oti_get(env);
+	struct lu_fid		*fid = &info->oti_fid;
+	struct inode		*inode;
+#endif /* LIXI */
+	int			 rc = 0;
+	ENTRY;
+
+	if (o->od_mnt != NULL)
+		RETURN(0);
+
+	if (strlen(dev) >= sizeof(o->od_mntdev))
+		RETURN(-E2BIG);
+	strncpy(o->od_mntdev, dev, sizeof(o->od_mntdev));
+
+	OBD_PAGE_ALLOC(__page, GFP_IOFS);
+	if (__page == NULL)
+		GOTO(out, rc = -ENOMEM);
+
+	str = lustre_cfg_string(cfg, 2);
+	s_flags = simple_strtoul(str, NULL, 0);
+	str = strstr(str, ":");
+	if (str)
+		lmd_flags = simple_strtoul(str + 1, NULL, 0);
+	opts = lustre_cfg_string(cfg, 3);
+	page = (unsigned long)page_address(__page);
+	options = (char *)page;
+	*options = '\0';
+	if (opts != NULL)
+		strncat(options, opts, option_size);
+
+	type = get_fs_type("btrfs");
+	if (!type) {
+		CERROR("%s: cannot find btrfs module\n", name);
+		GOTO(out, rc = -ENODEV);
+	}
+
+	o->od_mnt = vfs_kern_mount(type, s_flags, dev, options);
+	module_put(type->owner);
+
+	if (IS_ERR(o->od_mnt)) {
+		rc = PTR_ERR(o->od_mnt);
+		o->od_mnt = NULL;
+		CERROR("%s: can't mount %s: %d\n", name, dev, rc);
+		GOTO(out, rc);
+	}
+
+#ifdef HAVE_DEV_SET_RDONLY
+	if (dev_check_rdonly(o->od_mnt->mnt_sb->s_bdev)) {
+		CERROR("%s: underlying device %s is marked as read-only. "
+		       "Setup failed\n", name, dev);
+		GOTO(out_mnt, rc = -EROFS);
+	}
+#endif
+
+#ifdef LIXI
+	inode = osd_sb(o)->s_root->d_inode;
+	lu_local_obj_fid(fid, OSD_FS_ROOT_OID);
+	rc = osd_ea_fid_set(info, inode, fid, LMAC_NOT_IN_OI, 0);
+	if (rc != 0) {
+		CERROR("%s: failed to set lma on %s root inode\n", name, dev);
+		GOTO(out_mnt, rc);
+	}
+
+	if (lmd_flags & LMD_FLG_NOSCRUB)
+		o->od_noscrub = 1;
+#endif /* LIXI */
+
+	GOTO(out, rc = 0);
+
+#ifdef HAVE_DEV_SET_RDONLY /* LIXI */
+out_mnt:
+#endif /* LIXI */
+	mntput(o->od_mnt);
+	o->od_mnt = NULL;
+
+out:
+	if (__page)
+		OBD_PAGE_FREE(__page);
+
+	return rc;
+}
+
+static int osd_shutdown(const struct lu_env *env, struct osd_device *o)
+{
+	ENTRY;
+
+#ifdef LIXI
+	/* shutdown quota slave instance associated with the device */
+	if (o->od_quota_slave != NULL) {
+		qsd_fini(env, o->od_quota_slave);
+		o->od_quota_slave = NULL;
+	}
+
+	osd_fid_fini(env, o);
+#endif /* LIXI */
+
+	RETURN(0);
+}
+
+/*
+ * Concurrency: doesn't access mutable data
+ */
+static int osd_root_get(const struct lu_env *env,
+                        struct dt_device *dev, struct lu_fid *f)
+{
+        lu_local_obj_fid(f, OSD_FS_ROOT_OID);
+        return 0;
+}
+
+/*
+ * Concurrency: doesn't access mutable data.
+ */
+static void osd_conf_get(const struct lu_env *env,
+			 const struct dt_device *dev,
+			 struct dt_device_param *param)
+{
+#ifdef LIXI
+	struct osd_device *osd = osd_dt_dev(dev);
+#endif /* LIXI */
+
+	/* TODO LIXI: configure reasonable numbers */
+	/*
+	 * XXX should be taken from not-yet-existing fs abstraction layer.
+	 */
+#ifdef LIXI
+	param->ddp_max_name_len	= MAXNAMELEN;
+#else /* LIXI */
+	param->ddp_max_name_len	= 1024;
+#endif /* LIXI */
+	param->ddp_max_nlink	= 1 << 31; /* it's 8byte on a disk */
+	param->ddp_block_shift	= 12; /* XXX */
+	param->ddp_mount_type	= LDD_MT_BTRFS;
+
+#ifdef LIXI
+	param->ddp_mntopts	= MNTOPT_USERXATTR;
+	if (osd->od_posix_acl)
+		param->ddp_mntopts |= MNTOPT_ACL;
+	param->ddp_max_ea_size	= DXATTR_MAX_ENTRY_SIZE;
+#else /* LIXI */
+	param->ddp_mntopts	= 0;
+	param->ddp_max_ea_size	= 1024;
+#endif /* LIXI */
+
+	/* for maxbytes, report same value as ZPL */
+	param->ddp_maxbytes	= MAX_LFS_FILESIZE;
+
+	/* Default reserved fraction of the available space that should be kept
+	 * for error margin. Unfortunately, there are many factors that can
+	 * impact the overhead with zfs, so let's be very cautious for now and
+	 * reserve 20% of the available space which is not given out as grant.
+	 * This tunable can be changed on a live system via procfs if needed. */
+	param->ddp_grant_reserved = 20;
+
+	/* inodes are dynamically allocated, so we report the per-inode space
+	 * consumption to upper layers. This static value is not really accurate
+	 * and we should use the same logic as in udmu_objset_statfs() to
+	 * estimate the real size consumed by an object */
+	param->ddp_inodespace = 1024;
+	/* per-fragment overhead to be used by the client code */
+#ifdef LIXI
+	param->ddp_grant_frag = osd_blk_insert_cost();
+#else /* LIXI */
+	param->ddp_grant_frag = 6144;
+#endif /* LIXI */
+}
+
+/*
+ * Concurrency: shouldn't matter.
+ */
+int osd_statfs(const struct lu_env *env, struct dt_device *d,
+               struct obd_statfs *sfs)
+{
+        struct osd_device  *osd = osd_dt_dev(d);
+        struct super_block *sb = osd_sb(osd);
+        struct kstatfs     *ksfs;
+        int result = 0;
+
+	if (unlikely(osd->od_mnt == NULL))
+		return -EINPROGRESS;
+
+        /* osd_lproc.c call this without env, allocate ksfs for that case */
+        if (unlikely(env == NULL)) {
+                OBD_ALLOC_PTR(ksfs);
+                if (ksfs == NULL)
+                        return -ENOMEM;
+        } else {
+                ksfs = &osd_oti_get(env)->oti_ksfs;
+        }
+
+	spin_lock(&osd->od_osfs_lock);
+	result = sb->s_op->statfs(sb->s_root, ksfs);
+	if (likely(result == 0)) { /* N.B. statfs can't really fail */
+		statfs_pack(sfs, ksfs);
+		if (sb->s_flags & MS_RDONLY)
+			sfs->os_state = OS_STATE_READONLY;
+	}
+
+	spin_unlock(&osd->od_osfs_lock);
+
+	if (unlikely(env == NULL))
+                OBD_FREE_PTR(ksfs);
+
+#ifdef LIXI
+	/* Reserve a small amount of space for local objects like last_rcvd,
+	 * llog, quota files, ... */
+	if (sfs->os_bavail <= GRANT_FOR_LOCAL_OIDS) {
+		sfs->os_bavail = 0;
+	} else {
+		sfs->os_bavail -= GRANT_FOR_LOCAL_OIDS;
+		/** Take out metadata overhead for indirect blocks */
+		sfs->os_bavail -= sfs->os_bavail >> (sb->s_blocksize_bits - 3);
+	}
+#endif /* LIXI */
+
+        return result;
+}
+
+/*
+ * Concurrency: serialization provided by callers.
+ */
+static int osd_init_capa_ctxt(const struct lu_env *env, struct dt_device *d,
+                              int mode, unsigned long timeout, __u32 alg,
+                              struct lustre_capa_key *keys)
+{
+#ifdef LIXI
+	struct osd_device *dev = osd_dt_dev(d);
+#endif /* LIXI */
+	ENTRY;
+#ifdef LIXI
+	dev->od_fl_capa = mode;
+	dev->od_capa_timeout = timeout;
+	dev->od_capa_alg = alg;
+	dev->od_capa_keys = keys;
+#endif /* LIXI */
+	RETURN(0);
+}
+
+static const struct dt_device_operations osd_dt_ops = {
+#ifdef LIXI
+	.dt_root_get       = osd_root_get,
+	.dt_statfs         = osd_statfs,
+	.dt_trans_create   = osd_trans_create,
+	.dt_trans_start    = osd_trans_start,
+	.dt_trans_stop     = osd_trans_stop,
+	.dt_trans_cb_add   = osd_trans_cb_add,
+	.dt_conf_get       = osd_conf_get,
+	.dt_sync           = osd_sync,
+	.dt_ro             = osd_ro,
+	.dt_commit_async   = osd_commit_async,
+	.dt_init_capa_ctxt = osd_init_capa_ctxt,
+#else /* LIXI */
+	.dt_root_get       = osd_root_get,
+	.dt_statfs         = osd_statfs,
+	.dt_trans_create   = NULL,
+	.dt_trans_start    = NULL,
+	.dt_trans_stop     = NULL,
+	.dt_trans_cb_add   = NULL,
+	.dt_conf_get       = osd_conf_get,
+	.dt_sync           = NULL,
+	.dt_ro             = NULL,
+	.dt_commit_async   = NULL,
+	.dt_init_capa_ctxt = osd_init_capa_ctxt,
+#endif /* LIXI */
+};
+
+static int osd_device_init0(const struct lu_env *env,
+			    struct osd_device *o,
+			    struct lustre_cfg *cfg)
+{
+	struct lu_device	*l = osd2lu_dev(o);
+	struct osd_thread_info *info;
+	int			rc;
+	int			cplen = 0;
+
+	/* if the module was re-loaded, env can loose its keys */
+	rc = lu_env_refill((struct lu_env *) env);
+	if (rc)
+		GOTO(out, rc);
+	info = osd_oti_get(env);
+	LASSERT(info);
+
+	l->ld_ops = &osd_lu_ops;
+	o->od_dt_dev.dd_ops = &osd_dt_ops;
+
+	spin_lock_init(&o->od_osfs_lock);
+#ifdef LIXI
+	mutex_init(&o->od_otable_mutex);
+#endif /* LIXI */
+
+	o->od_capa_hash = init_capa_hash();
+	if (o->od_capa_hash == NULL)
+		GOTO(out, rc = -ENOMEM);
+
+#ifdef LIXI
+	o->od_read_cache = 1;
+	o->od_writethrough_cache = 1;
+	o->od_readcache_max_filesize = OSD_MAX_CACHE_SIZE;
+#endif /* LIXI */
+
+	cplen = strlcpy(o->od_svname, lustre_cfg_string(cfg, 4),
+			sizeof(o->od_svname));
+	if (cplen >= sizeof(o->od_svname)) {
+		rc = -E2BIG;
+		GOTO(out_capa, rc);
+	}
+
+	if (server_name_is_ost(o->od_svname))
+		o->od_is_ost = 1;
+
+	rc = osd_mount(env, o, cfg);
+	if (rc != 0)
+		GOTO(out_capa, rc);
+
+#ifdef LIXI
+	rc = osd_obj_map_init(env, o);
+	if (rc != 0)
+		GOTO(out_mnt, rc);
+#endif /* LIXI */
+
+	rc = lu_site_init(&o->od_site, l);
+	if (rc != 0)
+#ifdef LIXI
+		GOTO(out_compat, rc);
+#else /* LIXI */
+		GOTO(out_mnt, rc);
+#endif /* LIXI */
+	o->od_site.ls_bottom_dev = l;
+
+	rc = lu_site_init_finish(&o->od_site);
+	if (rc != 0)
+		GOTO(out_site, rc);
+
+#ifdef LIXI
+	/* self-repair LMA by default */
+	o->od_lma_self_repair = 1;
+
+	INIT_LIST_HEAD(&o->od_ios_list);
+	/* setup scrub, including OI files initialization */
+	rc = osd_scrub_setup(env, o);
+	if (rc < 0)
+		GOTO(out_site, rc);
+#endif /* LIXI */
+
+	rc = osd_procfs_init(o, o->od_svname);
+	if (rc != 0) {
+		CERROR("%s: can't initialize procfs: rc = %d\n",
+		       o->od_svname, rc);
+#ifdef LIXI
+		GOTO(out_scrub, rc);
+#else /* LIXI */
+		GOTO(out_site, rc);
+#endif /* LIXI */
+	}
+
+	LASSERT(l->ld_site->ls_linkage.next != NULL);
+	LASSERT(l->ld_site->ls_linkage.prev != NULL);
+
+#ifdef LIXI
+	/* initialize quota slave instance */
+	o->od_quota_slave = qsd_init(env, o->od_svname, &o->od_dt_dev,
+				     o->od_proc_entry);
+	if (IS_ERR(o->od_quota_slave)) {
+		rc = PTR_ERR(o->od_quota_slave);
+		o->od_quota_slave = NULL;
+		GOTO(out_procfs, rc);
+	}
+#endif /* LIXI */
+
+	RETURN(0);
+
+#ifdef LIXI
+out_procfs:
+	osd_procfs_fini(o);
+out_scrub:
+	osd_scrub_cleanup(env, o);
+#endif /* LIXI */
+out_site:
+	lu_site_fini(&o->od_site);
+#ifdef LIXI
+out_compat:
+	osd_obj_map_fini(o);
+#endif /* LIXI */
+out_mnt:
+	osd_umount(env, o);
+out_capa:
+	cleanup_capa_hash(o->od_capa_hash);
+out:
+	return rc;
+}
+
+static struct lu_device *osd_device_alloc(const struct lu_env *env,
+					  struct lu_device_type *t,
+					  struct lustre_cfg *cfg)
+{
+	struct osd_device *o;
+	int                rc;
+
+	OBD_ALLOC_PTR(o);
+	if (o == NULL)
+		return ERR_PTR(-ENOMEM);
+
+	rc = dt_device_init(&o->od_dt_dev, t);
+	if (rc == 0) {
+		/* Because the ctx might be revived in dt_device_init,
+		 * refill the env here */
+		lu_env_refill((struct lu_env *)env);
+		rc = osd_device_init0(env, o, cfg);
+		if (rc)
+			dt_device_fini(&o->od_dt_dev);
+	}
+
+	if (unlikely(rc != 0))
+		OBD_FREE_PTR(o);
+
+	return rc == 0 ? osd2lu_dev(o) : ERR_PTR(rc);
+}
+
+static struct lu_device *osd_device_free(const struct lu_env *env,
+					 struct lu_device *d)
+{
+	struct osd_device *o = osd_dev(d);
+	ENTRY;
+
+	cleanup_capa_hash(o->od_capa_hash);
+	/* XXX: make osd top device in order to release reference */
+	d->ld_site->ls_top_dev = d;
+	lu_site_purge(env, d->ld_site, -1);
+	if (!cfs_hash_is_empty(d->ld_site->ls_obj_hash)) {
+		LIBCFS_DEBUG_MSG_DATA_DECL(msgdata, D_ERROR, NULL);
+		lu_site_print(env, d->ld_site, &msgdata, lu_cdebug_printer);
+	}
+	lu_site_fini(&o->od_site);
+	dt_device_fini(&o->od_dt_dev);
+	OBD_FREE_PTR(o);
+	RETURN(NULL);
+}
+
+/*
+ * To be removed, setup is performed by osd_device_{init,alloc} and
+ * cleanup is performed by osd_device_{fini,free).
+ */
+static int osd_process_config(const struct lu_env *env,
+			      struct lu_device *d, struct lustre_cfg *cfg)
+{
+	struct osd_device		*o = osd_dev(d);
+	int				rc;
+	ENTRY;
+
+	switch (cfg->lcfg_command) {
+	case LCFG_SETUP:
+		rc = osd_mount(env, o, cfg);
+		break;
+	case LCFG_CLEANUP:
+		lu_dev_del_linkage(d->ld_site, d);
+		rc = osd_shutdown(env, o);
+		break;
+	case LCFG_PARAM:
+		LASSERT(&o->od_dt_dev);
+		rc = class_process_proc_param(PARAM_OSD, lprocfs_osd_obd_vars,
+					      cfg, &o->od_dt_dev);
+		if (rc > 0 || rc == -ENOSYS)
+			rc = class_process_proc_param(PARAM_OST,
+						      lprocfs_osd_obd_vars,
+						      cfg, &o->od_dt_dev);
+		break;
+	default:
+		rc = -ENOSYS;
+	}
+
+	RETURN(rc);
+}
+
+static int osd_recovery_complete(const struct lu_env *env, struct lu_device *d)
+{
+#ifdef LIXI
+	struct osd_device	*osd = osd_dev(d);
+	int			 rc = 0;
+	ENTRY;
+
+	if (osd->od_quota_slave == NULL)
+		RETURN(0);
+
+	/* start qsd instance on recovery completion, this notifies the quota
+	 * slave code that we are about to process new requests now */
+	rc = qsd_start(env, osd->od_quota_slave);
+	RETURN(rc);
+#else /* LIXI */
+	ENTRY;
+	RETURN(0);
+#endif /* LIXI */
+}
+
+/*
+ * we use exports to track all osd users
+ */
+static int osd_obd_connect(const struct lu_env *env, struct obd_export **exp,
+			   struct obd_device *obd, struct obd_uuid *cluuid,
+			   struct obd_connect_data *data, void *localdata)
+{
+	struct osd_device    *osd = osd_dev(obd->obd_lu_dev);
+	struct lustre_handle  conn;
+	int                   rc;
+	ENTRY;
+
+	CDEBUG(D_CONFIG, "connect #%d\n", osd->od_connects);
+
+	rc = class_connect(&conn, obd, cluuid);
+	if (rc)
+		RETURN(rc);
+
+	*exp = class_conn2export(&conn);
+
+	spin_lock(&osd->od_osfs_lock);
+	osd->od_connects++;
+	spin_unlock(&osd->od_osfs_lock);
+
+	RETURN(0);
+}
+
+/*
+ * once last export (we don't count self-export) disappeared
+ * osd can be released
+ */
+static int osd_obd_disconnect(struct obd_export *exp)
+{
+	struct obd_device *obd = exp->exp_obd;
+	struct osd_device *osd = osd_dev(obd->obd_lu_dev);
+	int                rc, release = 0;
+	ENTRY;
+
+	/* Only disconnect the underlying layers on the final disconnect. */
+	spin_lock(&osd->od_osfs_lock);
+	osd->od_connects--;
+	if (osd->od_connects == 0)
+		release = 1;
+	spin_unlock(&osd->od_osfs_lock);
+
+	rc = class_disconnect(exp); /* bz 9811 */
+
+	if (rc == 0 && release)
+		class_manual_cleanup(obd);
+	RETURN(rc);
+}
+
+static int osd_prepare(const struct lu_env *env, struct lu_device *pdev,
+		       struct lu_device *dev)
+{
+#ifdef LIXI
+	struct osd_device	*osd = osd_dev(dev);
+	int			 rc = 0;
+	ENTRY;
+
+	if (osd->od_quota_slave != NULL)
+		/* set up quota slave objects */
+		rc = qsd_prepare(env, osd->od_quota_slave);
+
+	RETURN(rc);
+#else /* LIXI */
+	ENTRY;
+	RETURN(0);
+#endif /* LIXI */
+}
+
+/*
+ * Btrfs OSD device type methods
+ */
+static int osd_type_init(struct lu_device_type *t)
+{
+	LU_CONTEXT_KEY_INIT(&osd_key);
+	return lu_context_key_register(&osd_key);
+}
+
+static void osd_type_fini(struct lu_device_type *t)
+{
+	lu_context_key_degister(&osd_key);
+}
+
+static void osd_type_start(struct lu_device_type *t)
+{
+}
+
+static void osd_type_stop(struct lu_device_type *t)
+{
+}
+
+static void *osd_key_init(const struct lu_context *ctx,
+			  struct lu_context_key *key)
+{
+	struct osd_thread_info *info;
+
+	OBD_ALLOC_PTR(info);
+	if (info != NULL)
+		info->oti_env = container_of(ctx, struct lu_env, le_ctx);
+	else
+		info = ERR_PTR(-ENOMEM);
+	return info;
+}
+
+static void osd_key_fini(const struct lu_context *ctx,
+			 struct lu_context_key *key, void *data)
+{
+	struct osd_thread_info *info = data;
+
+	OBD_FREE_PTR(info);
+}
+
+static void osd_key_exit(const struct lu_context *ctx,
+			 struct lu_context_key *key, void *data)
+{
+	struct osd_thread_info *info = data;
+
+	memset(info, 0, sizeof(*info));
+}
+
+struct lu_context_key osd_key = {
+	.lct_tags = LCT_DT_THREAD | LCT_MD_THREAD | LCT_MG_THREAD | LCT_LOCAL,
+	.lct_init = osd_key_init,
+	.lct_fini = osd_key_fini,
+	.lct_exit = osd_key_exit
+};
+
+static int osd_device_init(const struct lu_env *env, struct lu_device *d,
+			   const char *name, struct lu_device *next)
+{
+	struct osd_device *osd = osd_dev(d);
+
+	if (strlcpy(osd->od_svname, name, sizeof(osd->od_svname))
+	    >= sizeof(osd->od_svname))
+		return -E2BIG;
+	return osd_procfs_init(osd, name);
+}
+
+static struct lu_device *osd_device_fini(const struct lu_env *env,
+					 struct lu_device *d)
+{
+	struct osd_device *o = osd_dev(d);
+	ENTRY;
+
+	osd_shutdown(env, o);
+	osd_procfs_fini(o);
+#ifdef LIXI
+	osd_scrub_cleanup(env, o);
+	osd_obj_map_fini(o);
+#endif /* LIXI */
+	osd_umount(env, o);
+
+	RETURN(NULL);
+}
+
+const struct lu_device_operations osd_lu_ops = {
+#ifdef LIXI
+	.ldo_object_alloc      = osd_object_alloc,
+#else /* LIXI */
+	.ldo_object_alloc      = NULL,
+#endif /* LIXI */
+	.ldo_process_config    = osd_process_config,
+	.ldo_recovery_complete = osd_recovery_complete,
+	.ldo_prepare           = osd_prepare,
+};
+
+static const struct lu_device_type_operations osd_device_type_ops = {
+	.ldto_init = osd_type_init,
+	.ldto_fini = osd_type_fini,
+
+	.ldto_start = osd_type_start,
+	.ldto_stop  = osd_type_stop,
+
+	.ldto_device_alloc = osd_device_alloc,
+	.ldto_device_free  = osd_device_free,
+
+	.ldto_device_init    = osd_device_init,
+	.ldto_device_fini    = osd_device_fini
+};
+
+struct lu_device_type osd_device_type = {
+	.ldt_tags     = LU_DEVICE_DT,
+	.ldt_name     = LUSTRE_OSD_BTRFS_NAME,
+	.ldt_ops      = &osd_device_type_ops,
+	.ldt_ctx_tags = LCT_LOCAL,
+};
+
+static struct obd_ops osd_obd_device_ops = {
+	.o_owner       = THIS_MODULE,
+	.o_connect	= osd_obd_connect,
+	.o_disconnect	= osd_obd_disconnect,
+#ifdef LIXI
+	.o_fid_alloc	= osd_fid_alloc
+#endif /* LIXI */
+};
 
 static int __init osd_mod_init(void)
 {
-	CERROR("XXX init\n");
+	int rc;
+
+	rc = class_register_type(&osd_obd_device_ops, NULL, true, NULL,
+				 LUSTRE_OSD_BTRFS_NAME, &osd_device_type);
 	return 0;
 }
 
 static void __exit osd_mod_exit(void)
 {
-	CERROR("XXX exit\n");
+	class_unregister_type(LUSTRE_OSD_BTRFS_NAME);
 }
 
 MODULE_AUTHOR("DataDirect Networks, Inc. <http://www.ddn.com/>");
--- lustre/osd-btrfs/osd_internal.h
+++ lustre/osd-btrfs/osd_internal.h
@@ -51,4 +51,123 @@
 #include <linux/dirent.h>
 #include <linux/statfs.h>
 
+#include <dt_object.h>
+
+/*
+ * osd device.
+ */
+struct osd_device {
+	/* super-class */
+	struct dt_device          od_dt_dev;
+	/* information about underlying file system */
+	struct vfsmount          *od_mnt;
+	/* service name associated with the osd device */
+	char                      od_svname[MAX_OBD_NAME];
+	char                      od_mntdev[MAX_OBD_NAME];
+	int			  od_connects;
+	struct lu_site		  od_site;
+	spinlock_t		  od_osfs_lock;
+	struct hlist_head	 *od_capa_hash;
+	unsigned int              od_is_ost:1;
+	struct proc_dir_entry	 *od_proc_entry;
+};
+
+struct osd_thread_info {
+	const struct lu_env	*oti_env;
+	struct kstatfs		 oti_ksfs;
+};
+
+/*
+ * Helpers.
+ */
+extern const struct lu_device_operations  osd_lu_ops;
+
+static inline int lu_device_is_osd(const struct lu_device *d)
+{
+	return ergo(d != NULL && d->ld_ops != NULL, d->ld_ops == &osd_lu_ops);
+}
+
+#ifdef LIXI
+static inline struct osd_object *osd_obj(const struct lu_object *o)
+{
+	LASSERT(lu_device_is_osd(o->lo_dev));
+	return container_of0(o, struct osd_object, oo_dt.do_lu);
+}
+#endif /* LIXI */
+
+static inline struct osd_device *osd_dt_dev(const struct dt_device *d)
+{
+	LASSERT(lu_device_is_osd(&d->dd_lu_dev));
+	return container_of0(d, struct osd_device, od_dt_dev);
+}
+
+static inline struct osd_device *osd_dev(const struct lu_device *d)
+{
+	LASSERT(lu_device_is_osd(d));
+	return osd_dt_dev(container_of0(d, struct dt_device, dd_lu_dev));
+}
+
+#ifdef LIXI
+static inline struct osd_object *osd_dt_obj(const struct dt_object *d)
+{
+	return osd_obj(&d->do_lu);
+}
+
+static inline struct osd_device *osd_obj2dev(const struct osd_object *o)
+{
+	return osd_dev(o->oo_dt.do_lu.lo_dev);
+}
+#endif /* LIXI */
+
+static inline struct super_block *osd_sb(const struct osd_device *dev)
+{
+	return dev->od_mnt->mnt_sb;
+}
+
+static inline struct lu_device *osd2lu_dev(struct osd_device *osd)
+{
+	return &osd->od_dt_dev.dd_lu_dev;
+}
+
+#ifdef LIXI
+static inline struct objset *osd_dtobj2objset(struct dt_object *o)
+{
+	return osd_dev(o->do_lu.lo_dev)->od_objset.os;
+}
+
+static inline int osd_invariant(const struct osd_object *obj)
+{
+	return 1;
+}
+
+static inline int osd_object_invariant(const struct lu_object *l)
+{
+	return osd_invariant(osd_obj(l));
+}
+
+static inline struct seq_server_site *osd_seq_site(struct osd_device *osd)
+{
+	return osd->od_dt_dev.dd_lu_dev.ld_site->ld_seq_site;
+}
+
+static inline char *osd_name(struct osd_device *osd)
+{
+	return osd->od_dt_dev.dd_lu_dev.ld_obd->obd_name;
+}
+#endif /* LIXI */
+
+extern struct lu_context_key osd_key;
+
+static inline struct osd_thread_info *osd_oti_get(const struct lu_env *env)
+{
+	return lu_context_key_get(&env->le_ctx, &osd_key);
+}
+
+#ifdef LPROCFS
+/* osd_lproc.c */
+extern struct lprocfs_seq_vars lprocfs_osd_obd_vars[];
+int osd_procfs_init(struct osd_device *osd, const char *name);
+int osd_procfs_fini(struct osd_device *osd);
+#endif
+
 #endif /* _OSD_INTERNAL_H */
